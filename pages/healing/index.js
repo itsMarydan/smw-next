1e6f05cd5fd228cec533bed2a36034ba88f84348
Mary Olowu <olowu.marydan@gmail.com>
2022-02-03 18:42:29 -0600

    Have to work on form pages, inner healing, resources and events and activities

--- pages/healing/index.js
+++ pages/healing/index.js
@@ -2,6 +2,12 @@ import Banner from "../../components/banner";
 
 export default function Healing(){
     const bannerMessage ="Inner Healing";
+    const rotateText = [
+        "Just Breathe",
+        "Love is never Far",
+        "Let go of the past",
+        "Experience inner peace"
+    ]
     return(
         <>
             <Banner bannerMessage={bannerMessage} />
